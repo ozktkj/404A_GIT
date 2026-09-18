558766e5f7e18c11d4b8437ae73485a8c17fc4f7
ozktkj <ozktkj@gmail.com>
2026-09-18 17:03:09 +0900

    軽減税率対応

--- function.py
+++ function.py
@@ -1,2 +1,11 @@
-def add_tax(price):
-    return price * 1.1
+def add_tax(price,taxrate=0.1):
+    return int(price + price * taxrate)
+
+
+
+
+
+if __name__ == "__main__":
+    assert add_tax(1000) == 1100
+    assert add_tax(1000,0.08) == 1080
+    print("test")
